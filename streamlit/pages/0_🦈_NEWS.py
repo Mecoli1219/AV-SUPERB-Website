8f2901c0278d546be0452cc9a15564df58c9b54e
Mecoli <mecoli@Mecoli-MacBook-Pro.local>
2024-03-04 15:31:04 +0800

    Remove unused files

--- streamlit/pages/0_🦈_NEWS.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import time
-
-st.set_page_config(page_title="News", page_icon="📊")
-st.markdown("# News")
-st.sidebar.header("News")
